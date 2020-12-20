e19f3ad486dcf2d0a699139ebbe9e11ed51edd42
lupengliang <1559540409@qq.com>
2020-12-21 01:09:15 +0800

    update logging config commit

--- multi_function/logger/logging_setting.py
+++ multi_function/logger/logging_setting.py
@@ -6,9 +6,9 @@ import logging
 import os
 from logging import handlers
 
-# 定置打印日志
+# custom logging formatter
 class Logger(object):
-    level_relations = {  # 日志级别关系映射
+    level_relations = {  # relation of logging map
         'debug': logging.DEBUG,
         'info': logging.INFO,
         'warning': logging.WARNING,
@@ -19,16 +19,16 @@ class Logger(object):
     def __init__(self, filename, level='info', when='D', backCount=3,
                  fmt='%(asctime)s [%(filename)s][line:%(lineno)d]: %(message)s'):
        self.logger = logging.getLogger(filename)
-       format_str = logging.Formatter(fmt)  # 设置日志格式
-       self.logger.setLevel((self.level_relations.get(level)))  # 设置日志级别
-       self.logger.handlers.clear()  # 每次被调用后，清空已经存在handler
-       sh = logging.StreamHandler()  # 往屏幕上输出
-       sh.setFormatter(format_str)  # 设置日志显示的格式
+       format_str = logging.Formatter(fmt)  # setting log formatter
+       self.logger.setLevel((self.level_relations.get(level)))  # setting log level
+       self.logger.handlers.clear()  # first clear handle
+       sh = logging.StreamHandler()  # setting output in cmd
+       sh.setFormatter(format_str)  # setting logging formatter of you want to this
        th = handlers.TimedRotatingFileHandler(filename=filename, when=when, backupCount=backCount, encoding='utf-8')
-       th.setFormatter(format_str)  # 设置文件里写入的格式
-       self.logger.addHandler(sh)  # 把对象加到logger里
+       th.setFormatter(format_str)  # setting formatter of writing file
+       self.logger.addHandler(sh)  # add object to to logger
        self.logger.addHandler(th)
-       # self.logger.removeHandler(sh)  # 再次移除handles  根据实际情况进行取消注释
+       # self.logger.removeHandler(sh)  # second remove handle
 
 logger_obj = Logger('execute.log', level='debug')
 logger = logger_obj.logger
