7e23970264c4b1c0d0daa95365a7844d926fc278
lupengliang <1559540409@qq.com>
2021-05-07 23:06:38 +0800

    PyExecJS的基本使用

--- 数据解析/Mexecjs.py
+++ 数据解析/Mexecjs.py
@@ -3,4 +3,11 @@
 """
 import execjs
 
-execjs.eval('js的函数名')
+node = execjs.get()  # 实例化对象
+
+# js file
+file = 'test.js'
+ctx = node.compile(open(file, encoding='utf-8').read())  # 编译js代码
+result = ctx.eval('js的函数名')  # 执行js代码
+print(result)  # 打印结果
+
